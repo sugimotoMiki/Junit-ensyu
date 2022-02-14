d72ec39875086a0a916ea795ab62be31defb0460
Sugimoto Miki <miki.sugimoto@rakus-partners.co.jp>
2022-02-14 10:26:57 +0900

    e01単体テストを作成しました

--- src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -2,6 +2,26 @@ package junit.tutorial.ex01.e01;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 class StringUtilsTest {
 
+	
+	@Test
+	void snake1() {
+		StringUtils stringUtils = new StringUtils();
+		assertEquals("aaa", stringUtils.toSnakeCase("aaa"), "aaaと入力"); 
+	}
+	
+	@Test
+	void snake2() {
+		StringUtils stringUtils = new StringUtils();
+		assertEquals("hello_world", stringUtils.toSnakeCase("HelloWorld"));
+	}
+	
+	@Test
+	void snake3() {
+		StringUtils stringUtils = new StringUtils();
+		assertEquals("practice_junit", stringUtils.toSnakeCase("practiceJunit"));
+	}
+	
 }
